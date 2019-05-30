e50f81959473215b8a5e4f24850b44590c33ef00
Glenn Posadas <admin@glennvon.com>
2019-05-30 20:43:39 +0800

    ADD: Project source files. ADD: Pods.

---
+++ Movieee/Movieee/Utilitiies/MVKeys.swift
@@ -0,0 +1,17 @@
+//
+//  MVKeys.swift
+//  Movieee
+//
+//  Created by Glenn Von C. Posadas on 30/05/2019.
+//  Copyright © 2019 Glenn Von C. Posadas. All rights reserved.
+//
+
+import Foundation
+
+/// The struct that handles all the static keys of LLF.
+struct MVKeys {
+    struct tmDB {
+        static let dev = "5757951a7564a4cbe70474b2f598e722"
+        static let production = "5757951a7564a4cbe70474b2f598e722"
+    }
+}
